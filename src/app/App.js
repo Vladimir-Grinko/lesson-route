4bba0d7e0315c74b307a8e2871a97ca8472dbd0e
Vladimir <hewin2008@rambler.ru>
2021-11-01 17:00:06 +0300

    create react-router-dom

---
+++ src/app/App.js
@@ -0,0 +1,37 @@
+import NavBar from "./components/navBar";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Dashboard from "./components/dashboard";
+import Login from "./components/login";
+import Posts from "./components/posts";
+import Home from "./components/home";
+
+import NotFound from "./components/not-found";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <h1>App</h1>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        
+        <Route path="/dashboard/" component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route path="/posts/:postId?" component={Posts} />
+        <Route path="/404" component={NotFound} />
+        <Redirect to="/404" />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
+
+// при Switch не забывать упорядочивать маршруты внутри коммутатора от КОНКРЕТНЫХ к общим
+// exact применяем, если нужно точное совпадение с path, которое указали
+/* для компонентов, в которых нужно что-то изменить
+<Route
+          path="/dashboard"
+          render={(props) => <Dashboard isAdmin={false} {...props} />}
+        />
+*/
